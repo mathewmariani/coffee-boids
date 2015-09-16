f61204b6f796c7aa761733f95f84027574eb9655
matty <someone.mariani@gmail.com>
2015-09-16 16:59:10 -0400

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// @NOTE minify javascript files
+gulp.task('compress', function() {
+  return gulp.src('./javascript/**/*.js')
+    .pipe(uglify())
+
+    // @NOTE add .min.js ending
+    .pipe(rename({
+        extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./js'));
+});
 
 // @NOTE compile coffeescript into javascript
 gulp.task('coffee', function () {
     gulp.src('./_coffee/**/*.coffee')
         .pipe(coffee({bare: true}))
-        .pipe(gulp.dest('./js/'))
+        .pipe(gulp.dest('./javascript/'))
 });
 
 // @NOTE watch for changes in coffeescript files
+// @NOTE watch for changes in javascript files
 gulp.task('watch', function () {
     gulp.watch('_coffee/**/*.coffee', ['coffee']);
 });
 
+gulp.task('build', ['coffee', 'compress']);
+
 // @NOTE default gulp task
-gulp.task('default', ['coffee']);
+gulp.task('default', ['watch']);
